ee6a03552745599bc0bebcc5832a53a2b2afda52
A.J. Marchese <aj.marchese@outlook.com>
2023-03-03 08:22:26 -0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartyInvites.Models;
+
+using System;
+
+namespace PartyInvites.Controllers {
+    public class HomeController : Controller {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //The[HttpGet] attribute is used to specify that this action method should respond to HTTP GET requests.This means that the action method can only be accessed by making an HTTP GET request to the corresponding URL.
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        // Handles the form submission when a guest RSVPs to an event. It uses the HTTP POST method to send the form data to the server, and then stores the guest's response. Finally, it returns a view.
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse) {
+            //  takes a GuestResponse object that is populated from RsvpForm.cshtml view as a parameter and adds it to the repository responses list using the Add method.
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
+        }
+    }
+}
---
+++ Models/Repository.cs
@@ -0,0 +1,23 @@
+﻿// Declares a namespace called "PartyInvites.Models"
+namespace PartyInvites.Models
+{
+    // Declares a public static class called "Repository"
+    public static class Repository
+    {
+        // Declares a private static List object called "responses" that holds GuestResponse objects
+        private static List<GuestResponse> responses = new();
+
+        // Declares a public static property called "Responses" that returns the "responses" List object
+        public static IEnumerable<GuestResponse> Responses => responses;
+
+        // Declares a public static method called "AddResponse" that takes a GuestResponse object called "response" as input
+        public static void AddResponse(GuestResponse response)
+        {
+            // Writes the "response" object to the console
+            Console.WriteLine(response);
+
+            // Adds the "response" object to the "responses" List object
+            responses.Add(response);
+        }
+    }
+}
